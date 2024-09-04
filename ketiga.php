4dddb0c02a06a3fd58964bb9eb7111ef437e8e4c
wenynrf <h1101231044@student.untan.ac.id>
2024-09-05 05:00:08 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -4,6 +4,11 @@ class Lingkaran
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari)
+    {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function luas() : float {
         return self::PHI*$this->jari_jari*$this->jari_jari;
     }
@@ -20,6 +25,11 @@ class Bola
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari)
+    {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function volume() : float {
         return (4/3)*self::PHI*pow($this->jari_jari,3);
     }
@@ -31,6 +41,11 @@ class Tabung
     public $jari_jari;
     public $tinggi;
 
+    public function __construct($jari_jari, $tinggi)
+    {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
     public function volume() : float {
         return self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
@@ -42,43 +57,34 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
     
+    public function __construct($jari_jari, $tinggi)
+    {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return (1/3)*self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
 //Membuat Object nasi_tumpeng
-$nasi_tumpeng = new Kerucut();
-$nasi_tumpeng->jari_jari = 4;
-$nasi_tumpeng->tinggi = 10;
-$volume_nasi_tumpeng = $nasi_tumpeng -> volume();
-echo "Volume dari nasi tumpeng tersebut adalah {$volume_nasi_tumpeng} cm^3\n";
+$nasi_tumpeng = new Kerucut(4, 10);
+echo "Volume dari nasi tumpeng tersebut adalah {$nasi_tumpeng->volume()} cm^3\n";
 
 //Membuat Object dari Class Lingkaran
-$kls_lingkaran = new Lingkaran();
-$kls_lingkaran->jari_jari = 14;
-$luas_lingkaran = $kls_lingkaran->luas();
-$keliling_lingkaran = $kls_lingkaran->keliling();
-echo "Luas lingkaran tersebut adalah {$luas_lingkaran} cm^2\n";
-
-echo "Keliling lingkaran tersebut adalah {$keliling_lingkaran} cm\n";
+$kls_lingkaran = new Lingkaran(14);
+echo "Luas lingkaran tersebut adalah {$kls_lingkaran->luas()} cm^2\n";
+echo "Keliling lingkaran tersebut adalah {$kls_lingkaran->keliling()} cm\n";
 
 //Membuat Kode Implementasi Bola
-$bola = new Bola();
-$bola->jari_jari = 7;
-$volume_bola = $bola->volume();
-echo "Volume bola Susanto adalah {$volume_bola} cm^3\n";
+$bola = new Bola(7);
+echo "Volume bola Susanto adalah {$bola->volume()} cm^3\n";
 
 //Membuat Kode Implementasi Tabung
-$tabung = new Tabung();
-$tabung->jari_jari = 5;
-$tabung->tinggi = 10;
-$volume_tabung = $tabung->volume();
-echo "Volume tabung Andi adalah {$volume_tabung} cm^3\n";
+$tabung = new Tabung(5, 10);
+echo "Volume tabung Andi adalah {$tabung->volume()} cm^3\n";
 
 //Membuat Kode Implementasi Kerucut
-$kerucut = new Kerucut();
-$kerucut->jari_jari = 8;
-$kerucut->tinggi = 15;
-$volume_kerucut = $kerucut->volume();
-echo "Volume Kerucut Zahra adalah {$volume_kerucut} cm^3\n";
+$kerucut = new Kerucut(8, 15);
+echo "Volume Kerucut Zahra adalah {$kerucut->volume()} cm^3\n";
