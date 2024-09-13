a68aabc7e4251f90d346cde0bb944cc7c71df68f
wenynrf <h1101231044@student.untan.ac.id>
2024-09-13 14:10:19 +0700

    Praktikum 6B - Implementasi Namespace

--- HomePage.php
+++ HomePage.php
@@ -1,19 +1,31 @@
 <?php
+namespace App\Guest;
 class HomePage
 {
-    function tampil() : void {
+    public function tampil() : void {
         echo "Halaman depan guest";
     }
 }
 
 
+namespace App\Admin;
 class HomePage
 {
-    function tampil() : void {
+    public function tampil() : void {
         echo "Halaman depan admin";
     }
 }
 
 
-$hp = new HomePage();
+namespace App\Admin;
+class ManajemenArtikel
+{
+    public function tambah() : void
+    {
+        echo "Artikel berhasil ditambahkan";
+    }
+}
+
+
+$hp = new \App\Guest\HomePage();
 $hp->tampil();
